429d1e6c168876ce952d02264bdfdc412f295ff9
Anatoli Makarevich <makaroni4@gmail.com>
2023-12-30 23:18:40 +0100

    Specify SVG-s path in plugins

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    svgr(),
+    svgr({ include: '**/*.svg' }),
     react()
   ],
 })
