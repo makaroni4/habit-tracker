4a32bea7417c607fcbb59c78db8d4efda9006861
Anatoli Makarevich <makaroni4@gmail.com>
2023-12-30 22:21:20 +0100

    Added headline to tracker

--- src/components/HabitTracker.tsx
+++ src/components/HabitTracker.tsx
@@ -2,10 +2,14 @@ import HabitTable from "./HabitTable"
 import { useAppStore } from "../store"
 
 function HabitTracker() {
-  const { writingSectionsNumber } = useAppStore()
+  const { headlineEnabled, writingSectionsNumber } = useAppStore()
 
   return (
     <div className="print:absolute top-0 left-0 screen:h-[700px] print:h-screen overflow-hidden">
+      { headlineEnabled && (
+        <div className={`w-full h-16 mb-6 bg-slate-100`}></div>
+      )}
+
       <div className="mb-6">
         <HabitTable />
       </div>
