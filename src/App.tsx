6eceb4b0a7f1c935a02b41bbb52f6148c5b4109b
Anatoli Makarevich <makaroni4@gmail.com>
2023-12-30 12:28:56 +0100

    Added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import HabitTracker from "./components/HabitTracker"
 import Settings from "./components/Settings"
+import Logo from "./assets/logo.svg?react"
 
 function App() {
   return (
     <div className="screen:container p-8 print:p-0">
+      <header className="mb-8">
+        <Logo
+          className="h-8" />
+      </header>
+
       <Settings className="mb-8" />
 
       <div className="screen:p-4 screen:border-[1px] border-dashed border-slate-400 rounded-md">
