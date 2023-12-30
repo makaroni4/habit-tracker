8f42d8abba6396d94ad8ba75374496ff9a8e00d7
Anatoli Makarevich <makaroni4@gmail.com>
2023-12-30 23:13:45 +0100

    Add custom type declaration for SVG-s

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,2 +1,6 @@
+declare module '*.svg' {
+  const content: React.FC<React.SVGProps<SVGElement>>
+  export default content
+}
+
 /// <reference types="vite/client" />
-/// <reference types="vite-plugin-svgr/client" />
